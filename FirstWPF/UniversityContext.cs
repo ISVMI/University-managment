7651949a2173ccb12aace0d582b64f0e1e5b6bff
dasdsdas sdaasd <skorliak@mail.ru>
2025-01-04 16:39:18 +0400

    Dynamic server connection

--- FirstWPF/UniversityContext.cs
+++ FirstWPF/UniversityContext.cs
@@ -1,21 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace FirstWPF
 {
     public class UniversityContext : DbContext 
     {
         public UniversityContext(DbContextOptions<UniversityContext> options)
-            : base(options) {}
+            : base(options) 
+        {
+            Database.EnsureCreated();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-4CPJD5R;Database=UniversityDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server=(local);Database=UniversityDb;Trusted_Connection=True;TrustServerCertificate=True;");
         }
         public DbSet<University> Universities { get; set; }
         public DbSet<Group> Groups { get; set; }
